877882150339e10eb22025644b435c8acb5d893b
Lucas Kivi <lukekivi@gmail.com>
2022-03-24 23:37:00 -0500

    Added thrift file

--- app/pa2.thrift
+++ app/pa2.thrift
@@ -13,6 +13,7 @@ enum Status {
     ERROR = 1
 }
 
+
 /**
  * DHT join status of a node.
  */
@@ -23,6 +24,7 @@ enum JoinStatus {
     ERROR = 3
 }
 
+
 /**
  * Data need to access a node.
  */
@@ -32,6 +34,7 @@ struct NodeDetails {
     3: i32 port
 }
 
+
 /**
  * Model for a dictionary entry.
  */
@@ -39,3 +42,116 @@ struct Entry {
     1: string word,
     2: string definition
 }
+
+
+/**
+ * Finger entry for the finger table.
+ */
+struct Finger {
+    1: i32 start,
+    2: i32 last,
+    3: NodeDetails succ
+}
+
+
+/**
+ * Data structure that carries information about a nodes structure.
+ */
+struct NodeStructure {
+    1: i32 id,
+    3: i32 predId,
+    4: list<Entry> entries,
+    5: list<Finger> fingers
+}
+
+
+/**
+ * Simple status data structure.
+ */
+struct StatusData {
+    1: Status status,
+    2: string msg
+}
+
+
+/**
+ * Node for client data that a  SuperNode returns 
+ */
+struct NodeForClientData {
+    1: NodeDetails nodeInfo,
+    2: Status status,
+    3: string msg
+}
+
+
+/**
+ * Data about a node join operation
+ */
+struct NodeJoinData {
+    1: i32 id,
+    2: NodeDetails nodeInfo,
+    3: JoinStatus status,
+    4: string msg
+}
+
+
+/**
+ * Data bout the DHT as a whole
+ */
+struct DHTData {
+    1: list<NodeStructure> nodeStructures,
+    2: Status status,
+    3: string msg
+}
+
+
+/**
+ * The SuperNode of the DHT. This supernode is responsible for onboarding clients,
+ * nodes, and providing the client with the data about the DHT as a whole.
+ */
+service SuperNode {
+    NodeForClientData GetNodeForClient(),
+    NodeJoinData GetNodeForJoin(1: string ip, 2: i32 port),
+    StatusData PostJoin(),
+    DHTData GetDHTStructure()
+}
+
+
+/**
+ * Data for a Get() operation
+ */
+struct GetData {
+    1: string definition,
+    2: Status status,
+    3: string msg
+}
+
+
+/**
+ * Data returned by a node to represent its structure.
+ */
+struct NodeStructureData {
+    1: NodeStructure nodeStructure,
+    2: Status status,
+    3: string msg
+}
+
+/**
+ * The core entity in the DHT. Nodes are responsible for storing Entrys.
+ * Clients and Nodes can call Get() and Put() on them.
+ * The SuperNode can ask it for its structure.
+ * When new nodes enter the DHT they might update other nodes predecessors, 
+ * successors, and/or finger tables.
+ */
+service Node {
+    GetData Get(1: string word),
+    StatusData Put(1: string word, 2: string definition),
+    NodeStructureData GetNodeStructure(),
+    StatusData UpdatePredecessor(1: NodeDetails nodeInfo),
+    StatusData UpdateSuccessor(1: NodeDetails nodeInfo),
+    StatusData UpdateFingerTable()
+}
+
+
+
+
