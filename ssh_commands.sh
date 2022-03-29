dd68e8d8a4abc803237770b0d42147c4a15db38c
Lucas Kivi <lukekivi@gmail.com>
2022-03-29 01:16:23 -0500

    added ssh scripts

---
+++ ssh_commands.sh
@@ -0,0 +1,11 @@
+ssh kivix019@csel-kh4250-20.cselabs.umn.edu "cd ../../project/kivix019/Distributed-Dictionary/app; ant node"
+sleep 5
+ssh kivix019@csel-kh4250-21.cselabs.umn.edu "cd ../../project/kivix019/Distributed-Dictionary/app; ant node"
+sleep 5
+ssh kivix019@csel-kh4250-22.cselabs.umn.edu "cd ../../project/kivix019/Distributed-Dictionary/app; ant node"
+sleep 5
+ssh kivix019@csel-kh4250-23.cselabs.umn.edu "cd ../../project/kivix019/Distributed-Dictionary/app; ant node"
+sleep 5
+ssh kivix019@csel-kh4250-24.cselabs.umn.edu "cd ../../project/kivix019/Distributed-Dictionary/app; ant node"
+sleep 5
+ssh kivix019@csel-kh4250-25.cselabs.umn.edu "cd ../../project/kivix019/Distributed-Dictionary/app; ant node"
